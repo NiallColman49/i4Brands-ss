177a0736f0bce777740732dc3700fdf0ac4404c5
Niall <hello@niallcolman.co.uk>
2023-07-19 12:35:19 +0100

    Outline utilities created for fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,18 @@ module.exports = {
     theme: {
         container: {
             center: true,
-            padding: {
-                DEFAULT: "1rem",
-                sm: "2rem",
-                lg: "4rem",
-                xl: "5rem",
-                "2xl": "6rem",
+            padding: "1rem",
+            screens: {
+                sm: "640px",
+                md: "768px",
+                lg: "1024px",
+                xl: "1280px",
+                "2xl": "1280px",
             },
         },
         extend: {
             fontFamily: {
-                primary: "Whyte",
+                primary: "Whyte, sans-serif",
             },
             fontSize: {
                 xs: "0.625rem",
