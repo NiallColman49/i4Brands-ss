edb2927fccb7c7dc5c8edaa48f392ccf6688306e
Niall <hello@niallcolman.co.uk>
2023-07-27 23:03:47 +0100

    placeholder image on desktop

--- src/js/Modal.js
+++ src/js/Modal.js
@@ -2,6 +2,10 @@ let modals = [...document.querySelectorAll(".mod-list")];
 
 modals.forEach((mod, idx) => {
     mod.addEventListener("click", () => {
+        const placeholder = document.querySelector(".placeholder-image");
+
+        placeholder.classList.add("hidden");
+        console.log(placeholder);
         let panel = mod.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.classList.remove("active");
